50844dcdb1b477875097b3eafc4659dff4ae1024
kkumar-gcc <krishkumar9352@gmail.com>
2022-07-14 23:27:15 +0530

    make it better

--- dist/socket.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const serverConfig_1 = __importDefault(require("./serverConfig"));
-const socket_io_1 = require("socket.io");
-const socket = new socket_io_1.Server(serverConfig_1.default);
-module.exports = socket;
